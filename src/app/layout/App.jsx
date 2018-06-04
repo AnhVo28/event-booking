b7fc3ceb64b8b312cc1e05fb295ef205134b8393
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-06-04 16:18:01 +0300

    Add styled NavBar

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,15 +1,19 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
+import NavBar from "../../features/nav/navBar/NavBar";
 
 
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App" >
-        <h1> Re-event</h1>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App" >
+                <h1> Re-event</h1>
+                <NavBar/>
+                <EventDashboard/>
+            </div>
+        );
+    }
 }
 
 export default App;
