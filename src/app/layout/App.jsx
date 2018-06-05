a17e1bcdbd658a8267fac539941f198dc461732d
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-06-05 15:11:41 +0300

    Completed the static UI

--- src/app/layout/App.jsx
+++ src/app/layout/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { Container } from "semantic-ui-react";
 import EventDashboard from "../../features/event/eventDashboard/EventDashboard";
 import NavBar from "../../features/nav/navBar/NavBar";
 
@@ -7,10 +8,12 @@ import NavBar from "../../features/nav/navBar/NavBar";
 class App extends Component {
     render() {
         return (
-            <div className="App" >
-                <h1> Re-event</h1>
+            <div>
+
                 <NavBar/>
-                <EventDashboard/>
+                <Container className="main" >
+                    <EventDashboard/>
+                </Container>
             </div>
         );
     }
