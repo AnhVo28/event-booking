65165d3c60a6927e437c27fbbb4de84e420504f3
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-06-18 15:46:59 +0300

    Added toastr notification when updating and creating event

--- src/index.js
+++ src/index.js
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import './index.css';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
 import { configureStore } from './app/store/configureStore';
 import 'semantic-ui-css/semantic.min.css';
 import App from './app/layout/App.jsx';
 import ScollToTop from './app/common/util/SrollToTop';
 import registerServiceWorker from './registerServiceWorker';
 import { loadEvents } from './features/event/eventActions';
+import ReduxToastr from 'react-redux-toastr';
+
 
 const store = configureStore();
 store.dispatch(loadEvents());
@@ -17,6 +20,11 @@ ReactDOM.render(
     <Provider store={store}>
         <BrowserRouter>
             <ScollToTop>
+                <ReduxToastr
+                    timeOut= {3000}
+                    position='bottom-right'
+                    transitionIn='fadeIn'
+                    transitionOut='fadeOut'/>
                 <App />
             </ScollToTop>
         </BrowserRouter>
