105d3ce67f4cce81006c961fa3b5019a68f2ad6f
Anh Hoang Vo <vohoanganhkhtn@gmail.com>
2018-06-26 15:23:56 +0300

    Added photoPage and Dopzone comp to display uploaded img

--- src/features/user/Settings/PhotoPage.jsx
+++ src/features/user/Settings/PhotoPage.jsx
@@ -1,11 +1,104 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {
+    Image,
+    Segment,
+    Header,
+    Divider,
+    Grid,
+    Button,
+    Card,
+    Icon
+} from 'semantic-ui-react';
+import Dropzone from 'react-dropzone';
 
-const PhotoPage = () => {
-    return (
-        <div>
-            <h2>Photo Page</h2>
-        </div>
-    );
-};
+class PhotosPage extends Component {
+    state = {
+        files: [],
+        fileName: ''
+    };
 
-export default PhotoPage;
+    onDrop = files => {
+        this.setState({
+            files,
+            fileName: files[0].name
+        });
+    };
+
+    render() {
+        if (this.state.files[0]) {
+            console.log(this.state.files[0]);
+        }
+        return (
+            <Segment>
+                <Header dividing size="large" content="Your Photos" />
+                <Grid>
+                    <Grid.Row />
+                    <Grid.Column width={4}>
+                        <Header color="teal" sub content="Step 1 - Add Photo" />
+                        <Dropzone onDrop={this.onDrop} multiple={false}>
+                            <div
+                                style={{
+                                    paddingTop: '30px',
+                                    textAlign: 'center'
+                                }}
+                            >
+                                <Icon name="upload" size="huge" />
+                                <Header
+                                    style={{ padding: '0px 2px' }}
+                                    content="Drop image here or click to add"
+                                />
+                            </div>
+                        </Dropzone>
+                    </Grid.Column>
+                    <Grid.Column width={1} />
+                    <Grid.Column width={4}>
+                        <Header
+                            sub
+                            color="teal"
+                            content="Step 2 - Resize image"
+                        />
+                    </Grid.Column>
+                    <Grid.Column width={1} />
+                    <Grid.Column width={4}>
+                        <Header
+                            sub
+                            color="teal"
+                            content="Step 3 - Preview and Upload"
+                        />
+                        {this.state.files[0] && (
+                            <Image
+                                style={{
+                                    minHeight: '200px',
+                                    minWidth: '200px'
+                                }}
+                                src={this.state.files[0].preview}
+                            />
+                        )}
+                    </Grid.Column>
+                </Grid>
+
+                <Divider />
+                <Header sub color="teal" content="All Photos" />
+
+                <Card.Group itemsPerRow={5}>
+                    <Card>
+                        <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
+                        <Button positive>Main Photo</Button>
+                    </Card>
+
+                    <Card>
+                        <Image src="https://randomuser.me/api/portraits/men/20.jpg" />
+                        <div className="ui two buttons">
+                            <Button basic color="green">
+                                Main
+                            </Button>
+                            <Button basic icon="trash" color="red" />
+                        </div>
+                    </Card>
+                </Card.Group>
+            </Segment>
+        );
+    }
+}
+
+export default PhotosPage;
